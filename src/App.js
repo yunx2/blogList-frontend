053651e493ffe616fd53b23db5e721aaefe4010a
Yunyun <celeph4is@gmail.com>
2020-06-03 12:40:52 -0700

    exercise 5.1 complete, add login functionality from front end, conditionally render components based on whether user is logged in or not

---
+++ src/App.js
@@ -0,0 +1,69 @@
+import React, { useState, useEffect } from 'react'
+import Blog from './components/Blog'
+import blogService from './services/blogs'
+import loginService from './services/login'
+
+const App = () => {
+  const [blogs, setBlogs] = useState([])
+  const  [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
+
+  useEffect(() => {
+    blogService.getAll().then(blogs =>
+      setBlogs( blogs )
+    )  
+  }, [])
+
+  const handleLogin = async e => {
+    e.preventDefault()
+    try {
+      const user = await loginService.login({ username, password
+      })
+      setUser(user)
+      setUsername('')
+      setPassword('')
+    } catch (exception) {
+      console.error('wrong credentials')
+    }
+  }
+
+  const loginForm = () => (
+    <form onSubmit={handleLogin}>
+      <div>
+        username
+        <input 
+        type="text" 
+        value={username} 
+        name="Username"
+        onChange={({ target }) => setUsername(target.value)} />
+      </div>
+      <div>
+        password
+        <input 
+        type="password" 
+        value={password} 
+        name="Password" 
+        onChange={({ target }) => setPassword(target.value)} />
+      </div>
+      <button type="submit">login</button>
+    </form>
+  )
+
+  const blogList = () => (
+    <div>
+      <h2>blogs</h2>
+      {blogs.map(blog =>
+        <Blog key={blog.id} blog={blog} />
+      )}
+    </div>
+  )
+  
+  return (
+    <div>
+      {user === null ? loginForm() : blogList()}
+    </div>
+  )
+}
+
+export default App
