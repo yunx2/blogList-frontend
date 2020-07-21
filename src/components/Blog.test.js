6f649d9d076adacaa7c72f9798ab0772c1968913
Yunyun <celeph4is@gmail.com>
2020-07-21 16:23:03 -0700

    exercise 5.15: test to show that clicking 'like' button twice, calls event handler twice

--- src/components/Blog.test.js
+++ src/components/Blog.test.js
@@ -1,21 +1,39 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
+import { prettyDOM } from '@testing-library/dom'
 import Blog from './Blog'
 
+const blog = {
+  title: 'title title title',
+  author: 'author name',
+  url: 'favoriteblog.com',
+  likes: '2'
+}
+
 test('renders content, contains text about title and author, does not contain text about url and likes', () => {
-  const blog = {
-    title: 'title title title',
-    author: 'author name',
-    url: 'favoriteblog.com',
-    likes: '2'
-  }
+
   const component = render(
-    <Blog blog={blog} />
+    <Blog favorite={blog} />
   )
   component.debug()
   expect(component.container).toHaveTextContent('title title title')
   expect(component.container).toHaveTextContent('author name')
-  expect(component.container).not.toHaveTextContent('.com')
-  expect(component.container).not.toHaveTextContent('2')
 })
+
+test('clicking "like" button twice calls the event handler component receives as props twice', () => {
+  const mockHandler = jest.fn()
+  const blogComponent = render(
+    <Blog favorite={blog} updateBlog={mockHandler} />
+  )
+  const likeButton = blogComponent.getByText('like')
+  fireEvent.click(likeButton)
+  fireEvent.click(likeButton)
+
+  expect(mockHandler.mock.calls).toHaveLength(2)
+})
+
+// test('clicking button displays url and likes information', () => {
+
+
+// })
