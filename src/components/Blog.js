7d29c6672960bd86f00454e872f8412e432b0b26
Yunyun <celeph4is@gmail.com>
2020-09-01 18:18:18 -0700

    add bootstrap

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -9,23 +9,12 @@ const Blog = ({ id }) => {
     return state.blogs.find(blog => blog.id === id)
   })
   // console.log('blog:', blog)
-
   const blogStyle = {
     paddingTop: 10,
     paddingLeft: 2,
     border: 'solid',
     borderWidth: 1,
     marginBottom: 5
-  }
-  const dispatch = useDispatch()
-
-  const handleLike = () => {
-    const dataObject = {
-      ...blog,
-      likes: blog.likes + 1
-    }
-    // update(blog.id, dataObject)
-    dispatch(likeBlog(dataObject))
   }
 
   return (
